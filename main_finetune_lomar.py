f1a6489c3caf693b963ce75d2e27e834eec9b304
Jun Chen <jun.chen@kaust.edu.sa>
2022-06-18 18:08:02 +0300

    Add files via upload

--- main_finetune_lomar.py
+++ main_finetune_lomar.py
@@ -323,10 +323,10 @@ def main(args):
             log_writer=log_writer,
             args=args
         )
-        #if args.output_dir:
-        #    misc.save_model(
-        #        args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-        #        loss_scaler=loss_scaler, epoch=epoch)
+        if args.output_dir:
+           misc.save_model(
+               args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
+               loss_scaler=loss_scaler, epoch=epoch)
 
         test_stats = evaluate(data_loader_val, model, device)
         print(f"Accuracy of the network on the {len(dataset_val)} test images: {test_stats['acc1']:.1f}%")
